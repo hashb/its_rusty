9ef791274273f581d9bfd0960e8e6fd697bf6ff7
Chenna Kautilya <chenna@outlook.com>
2022-02-05 16:10:49 -0800

    tests for gcd

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,4 @@
+mod find_gcd;
 mod hello;
 mod hello_parallel;
 mod hello_println;
@@ -6,4 +7,18 @@ fn main() {
     hello::hello();
     hello_println::hello_println();
     hello_parallel::hello_parallel();
+    let n: u64 = 20;
+    let m: u64 = 12;
+    println!("gcd({},{}) = {}", n, m, find_gcd::gcd(n, m));
+    // println!("gcd({},{}) = {}", n, 0, find_gcd::gcd(n, 0));  // causes panic
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(find_gcd::gcd(14, 15), 1);
+
+    assert_eq!(
+        find_gcd::gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19),
+        3 * 11
+    );
 }
